88685a4b796141909b7b8f7646ae2ef3f30d42f9
peter <peter.k.nets@gmail.com>
2016-04-04 13:42:22 +0300

    Integrate contact-splash into contact-content

--- src/components/page-contact.js
+++ src/components/page-contact.js
@@ -4,12 +4,17 @@ class PageContact extends Component {
   render() {
     return (
       <div id="contact">
-        <div className="splash">
-          <div className="splash-title">
+        <div className="content">
+          <div className="content-title">
             <h1>Reach Out</h1>
           </div>
-        </div>
-        <div className="content">
+          <div>
+            <p>
+              If you have any questions or wish to chat about
+              universe and all other interesting things,
+              feel free to write me a couple of lines.
+            </p>
+          </div>
         </div>
       </div>
     );
