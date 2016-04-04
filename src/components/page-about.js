d269f6a81d0541ef902f1fc480625a15f9fc635f
peter <peter.k.nets@gmail.com>
2016-04-04 09:42:01 +0300

    Add some temporary content to about-content element

--- src/components/page-about.js
+++ src/components/page-about.js
@@ -10,6 +10,21 @@ class PageAbout extends Component {
           </div>
         </div>
         <div className="content">
+          <div className="left-column">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit 
+              esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt
+              in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className="right-column">
+            <img src="src/img/about-avatar.jpg" alt="Photo of Peter Krevenets" />
+          </div>
         </div>
       </div>
     );
