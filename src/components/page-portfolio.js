5376e7541fa9151eacf3d2c2a4eacc9cd7d9f79c
peter <peter.k.nets@gmail.com>
2016-04-04 13:34:06 +0300

    Restyle content-titles

--- src/components/page-portfolio.js
+++ src/components/page-portfolio.js
@@ -11,6 +11,7 @@ class PagePortfolio extends Component {
           <ul>
             <li>
               <a href="http://codepen.io/GuRuGu/full/ONPXKb/" target="_blank">
+                <img src="src/img/portfolio-content1_2.jpg" label="Stephen King Tribute Page" />
               </a>
             </li>
           </ul>
