42a001d1fa8b86429ec0acc10c8ae482b65c6c4a
peter <peter.k.nets@gmail.com>
2016-04-05 09:54:40 +0300

    Start restyling portfolio-section

--- src/components/page-portfolio.js
+++ src/components/page-portfolio.js
@@ -8,13 +8,32 @@ class PagePortfolio extends Component {
           <div className="content-title">
             <h1>Portfolio</h1>
           </div>
-          <ul>
-            <li>
-              <a href="http://codepen.io/GuRuGu/full/ONPXKb/" target="_blank">
-                <img src="src/img/portfolio-content1_2.jpg" label="Stephen King Tribute Page" />
-              </a>
-            </li>
-          </ul>
+          <div className="left-column">
+            <a href="http://codepen.io/GuRuGu/full/ONPXKb/" target="_blank">
+              <img src="src/img/portfolio-content1_2.jpg" label="Stephen King Tribute Page" />
+            </a>
+          </div>
+          <div className="right-column">
+            <h4>Title:</h4>
+            <p>Stephen King Tribute Page</p>
+            <h4>Description:</h4>
+            <p>
+              A simple page designed in order to test my skills
+              and complete a Free Code Camp task. At first I didn&#39;t
+              know where to start and just copying a provided example
+              was too boring. Then one day I saw a nice sliding animation
+              and something clicked - finally there was something interesting
+              I wanted to try. As for Stephen King - I read so much
+              of his books when in my teens, so it was an easy choice.
+            </p>
+            <h4>Technologies:</h4>
+            <ul>
+              <li>JavaScript</li>
+              <li>Jquery</li>
+              <li>Bootstrap</li>
+              <li>HTML/CSS</li>
+            </ul>
+          </div>
         </div>
       </div>
     );
