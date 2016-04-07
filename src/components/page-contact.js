e0dc2247212b233fa21ec4a779c6ccbe5deea37b
peter <peter.k.nets@gmail.com>
2016-04-07 11:43:09 +0300

    First draft of contact-form

--- src/components/page-contact.js
+++ src/components/page-contact.js
@@ -9,23 +9,34 @@ class PageContact extends Component {
             <h2>CONTACT</h2>
           </div>
           <div className="left-column">
+            <form>
+              <div>
+                <label htmlFor="form-name">Name:</label>
+                <input id="form-name" type="text" placeholder="Name" />
+              </div>
+              <div>
+                <label htmlFor="form-email">Email address:</label>
+                <input id="form-email" type="text" placeholder="Email address" />
+              </div>
+              <div>
+                <label htmlFor="form-message">Message:</label>
+                <textarea rows="5" id="form-message" placeholder="Message" />
+              </div>
+              <button type="submit">Send</button>
+            </form>
+          </div>
+          <div className="right-column">
             <p>
               If you have any questions or propositions,
               please do not hesitate to contact me!
             </p>
             <p>
-              You can use the following email address:
+              The contact form to the left is currently
+              not functioning, but you can use email:
             </p>
             <p>
               <i className="fa fa-envelope"></i>
               <a href="mailto:peter.k.nets@gmail.com">peter.k.nets@gmail.com</a>
-            </p>
-          </div>
-          <div className="right-column">
-            <p>
-              Later on this is where the contact form is going to be.
-              As for now please use other means of communication
-              listed here.
             </p>
           </div>
         </div>
