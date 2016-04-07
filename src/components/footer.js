904fce7c81e7f5c6817aa662511e0c0887ad49a2
peter <peter.k.nets@gmail.com>
2016-04-07 10:32:33 +0300

    Make a bootstrap-like footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,11 +6,17 @@ class Footer extends Component {
       <div id="footer">
         <div className="left-column">
           <h3>ABOUT THIS PAGE</h3>
+          <p>Created with a lot of music and coffee by Peter.</p>
         </div>
         <div className="right-column">
           <h3>AROUND THE WEB</h3>
+          <ul>
+            <li><a href="https://twitter.com/peter_krevenets" target="_blank"><i className="fa fa-twitter-square fa-2x"></i></a></li>
+            <li><a href="https://ua.linkedin.com/in/peter-krevenets-35b50b33" target="_blank"><i className="fa fa-linkedin-square fa-2x"></i></a></li>
+            <li><a href="https://github.com/GuRuGuMaWaRu" target="_blank"><i className="fa fa-github-square fa-2x"></i></a></li>
+          </ul>
         </div>
-        <p>&copy; Peter Krevenets, 2016.</p>
+        <p className="copyright">&copy; Peter Krevenets, 2016.</p>
       </div>
     );
   }
