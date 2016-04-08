4be1203dd08e964574d813339b6bafc1154f24d9
peter <peter.k.nets@gmail.com>
2016-04-08 10:07:40 +0300

    Additional restyling for contact-section; add warning messages

--- src/components/page-contact.js
+++ src/components/page-contact.js
@@ -8,26 +8,31 @@ class PageContact extends Component {
           <div className="content-title">
             <h2>CONTACT</h2>
           </div>
+
           <div className="left-column">
             <form id="contactForm" noValidate>
               <div>
                 <span></span>
                 <label htmlFor="form-name">Name:</label>
               </div>
-                <input id="form-name" type="text" required />
+                <input id="form-name" type="text" name="name" required />
+                <p className="warning-message">Please enter your name</p>
               <div>
                 <span></span>
                 <label htmlFor="form-email">Email address:</label>
               </div>
-                <input id="form-email" type="text" required />
+                <input id="form-email" type="email" name="email" required />
+                <p className="warning-message">Please enter your email address</p>
               <div>
                 <span></span>
                 <label htmlFor="form-message">Message:</label>
               </div>
-                <textarea rows="4" id="form-message" required />
+                <textarea rows="4" id="form-message" name="message" required />
+                <p className="warning-message">Please enter your message</p>
               <button type="submit">Send</button>
             </form>
           </div>
+
           <div className="right-column">
             <p>
               If you have any questions or propositions,
@@ -35,11 +40,13 @@ class PageContact extends Component {
             </p>
             <p>
               The contact form to the left is currently
-              not functioning, but you can use email:
+              not functioning, so you can send an email to:
             </p>
             <p>
-              <i className="fa fa-envelope"></i>
-              <a href="mailto:peter.k.nets@gmail.com">peter.k.nets@gmail.com</a>
+              <a href="mailto:peter.k.nets@gmail.com">
+                <i className="fa fa-envelope"></i>
+                peter.k.nets@gmail.com
+              </a>
             </p>
           </div>
         </div>
