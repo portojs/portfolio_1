a21668dce035ff1764b09cc7a8ee7547a19e4ce1
peter <peter.k.nets@gmail.com>
2016-04-12 13:48:24 +0300

    Attempt to add button-highlight functionality

--- src/index.js
+++ src/index.js
@@ -24,11 +24,36 @@ class App extends Component {
   }
 }
 
-// page scrolling feature
+// scroll to a page section after nav-button click
 $('body').on('click', '.nav-bar a', function(ev) {
   ev.preventDefault();
   const $anchor = $(this);
   $('body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1000);
+});
+
+// nav-button is highlighted when a corresponding page section is reached
+$(window).on('scroll', function() {
+  const windowOffset = $(window).scrollTop(),
+        aboutOffset = $('#about').offset().top,
+        portfolioOffset = $('#portfolio').offset().top,
+        contactOffset = $('#contact').offset().top,
+        aboutButton = $('.contact').find('i'),
+        portfolioButton = $('.portfolio').find('i'),
+        contactButton = $('.contact').find('i');
+  console.log(windowOffset, portfolioOffset);
+  if (windowOffset > contactOffset) {
+    contactButton.addClass('highlight');
+    portfolioButton.removeClass('highlight');
+    aboutButton.removeClass('highlight');
+  } else if (windowOffset > portfolioOffset) {
+    portfolioButton.addClass('highlight');
+    contactButton.removeClass('highlight');
+    aboutButton.removeClass('highlight');
+  } else if (windowOffset > aboutOffset) {
+    aboutButton.addClass('highlight');
+    contactButton.removeClass('highlight');
+    portfolioButton.removeClass('highlight');
+  }
 });
 
 ReactDOM.render(
